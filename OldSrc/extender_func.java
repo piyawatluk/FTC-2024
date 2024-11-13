35a7271e85db090ce21829b85c6a20becc9461eb
piyawat luknatin <125401439+piyawatluk@users.noreply.github.com>
2024-11-13 17:52:22 +0700

    Update extender_func.java

--- OldSrc/extender_func.java
+++ OldSrc/extender_func.java
@@ -9,17 +9,19 @@ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="DC_exteder_test", group="Iterative OpMode")
 public class TEST2 extends OpMode
+
 {
+
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor extender = null;
     @Override
     public void init() {
-        
+
         telemetry.addData("Status", "Initialized");
         extender = hardwareMap.get(DcMotor.class, "et");
-        extender.setDirection(DcMotor.Direction.FORWARD);
+        extender.setDirection(DcMotor.Direction.REVERSE);
         telemetry.addData("Status", "Initialized");
-        
+
     }
 
     @Override
@@ -30,31 +32,44 @@ public class TEST2 extends OpMode
     public void start() {
         runtime.reset();
     }
-    
+
     public void extender_func(){
         int max_pos = 6200;
         int min_pos = 700;
         double power;
         double drive = -gamepad1.left_stick_y;
-        int pos = extender.getCurrentPosition();
-        power = drive;
-        
-                
-        if ((power > 0 && pos < max_pos) || (power < 0 && pos > min_pos)) {
-            
-            extender.setPower(power);
-            
-        } 
-        
-        else {
-            
-            extender.setPower(0);
+
+        if (drive > 0){
+            power = 1;
+        }
+        else if (drive < 0){
+            power = -1;
         }
 
-        
+        else {
+            power = 0;
+        }
+
+        int pos = extender.getCurrentPosition();
+
+
+        if ((power > 0 && pos < max_pos) || (power < 0 && pos > min_pos)) {
+
+            extender.setPower(power);
+
+        }
+
+        else {
+
+            extender.setPower(0);
+
+        }
+
+
         //extender.setPower(power);
-        
-        
+
+
+        telemetry.addData("input (%.2f)", drive);
         telemetry.addData("Motors", "Power (%.2f)", power);
         telemetry.addData("Pos", pos);
         telemetry.update();
