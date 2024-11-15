692905c21d03af08313298aa73e71f899b3efd5d
localenstarrie <peal.sima@gmail.com>
2024-11-15 17:11:10 +0700

    oldsrc to teamcode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test3.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="spinneyyyngh", group="Iterative OpMode")
+public class Test3 extends OpMode
+
+{
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor extender = null;
+    @Override
+    public void init() {
+
+        telemetry.addData("Status", "Initialized");
+        extender = hardwareMap.get(DcMotor.class, "et");
+        extender.setDirection(DcMotor.Direction.FORWARD);
+        telemetry.addData("Status", "Initialized");
+
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    public void extender_func(){
+        double power;
+
+
+        if (gamepad1.a == true){
+            power = 1;
+        }
+        else if (gamepad1.b == true){
+            power = -1;
+        }
+
+        else {
+            power = 0;
+        }
+
+
+        extender.setPower(power);
+
+
+        telemetry.addData("Motors", "Power (%.2f)", power);
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        extender_func();
+    }
+    @Override
+    public void stop() {
+    }
+
+}
