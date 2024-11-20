890ef8a779f1d2dc2e44d30fd993a3c4736698c5
localenstarrie <peal.sima@gmail.com>
2024-11-20 18:42:54 +0700

    add sv_4 power 0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_arm.java
@@ -28,7 +28,6 @@ public class test_arm extends OpMode
 
         sv_1.setPosition(servo_pos);
         sv_3.setPosition(servo_pos);
-        sv_4.setPower(0);
 
         telemetry.addData("Status", "Initialized");
     }
@@ -51,6 +50,9 @@ public class test_arm extends OpMode
         }
         else if(gamepad1.left_trigger > 0.1){
             sv_4.setPower(out_speed);
+        }
+        else{
+            sv_4.setPower(0);
         }
 
         telemetry.addData("Arm Position", servo_pos);
