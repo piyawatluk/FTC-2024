a370a780d77f2b99b6ca98335d15ef349473c82a
piyawat luknatin <125401439+piyawatluk@users.noreply.github.com>
2024-11-27 15:17:25 +0700

    disable test_arm.java for full system test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_arm.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -7,6 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Disabled
 @TeleOp(name = "Arm", group = "Iterative OpMode")
 public class test_arm extends OpMode
 
