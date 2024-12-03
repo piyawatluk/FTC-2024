523260fd0f3c6375672facdd5832099442723950
localenstarrie <peal.sima@gmail.com>
2024-12-03 17:51:09 +0700

    gripper open 0.3 -> 0.15

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -179,7 +179,7 @@ public class teleop extends OpMode
         if (gamepad1.right_bumper){
             sv_2.setPosition(0);
         } else if (gamepad1.right_trigger > 0.5) {
-            sv_2.setPosition(0.3);
+            sv_2.setPosition(0.15);
         }
     }
 
