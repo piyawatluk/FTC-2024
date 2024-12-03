c43a978bcb25e645e8441ab777cecfd07f73767b
piyawat luknatin <125401439+piyawatluk@users.noreply.github.com>
2024-12-03 09:12:50 +0700

    servo position update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm_func_auto.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class arm_func_auto {
+
+}
