dff47f27b368b4411e9e94a01875f9215aab8b92
piyawat luknatin <125401439+piyawatluk@users.noreply.github.com>
2024-12-06 13:15:15 +0700

    update auto path right

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_red_right.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto_red_right.java
@@ -15,9 +15,10 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 @Autonomous(name = "red_right")
 public class auto_red_right extends LinearOpMode {
     private Servo sv_1, sv_3, sv_2;
-    private CRServo sv_4;
     private DcMotor extender_L = null;
     private DcMotor extender_R = null;
+    private DigitalChannel ls = null;
+
     public boolean rightmotor = true;
     @Override
     public void runOpMode() throws InterruptedException {
@@ -28,10 +29,15 @@ public class auto_red_right extends LinearOpMode {
         extender_R = hardwareMap.get(DcMotor.class,  "et_2");
         extender_L.setDirection(DcMotor.Direction.FORWARD);
         extender_R.setDirection(DcMotor.Direction.REVERSE);
-        extender_L.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        extender_R.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        extender_L.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        extender_R.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ls = hardwareMap.get(DigitalChannel.class, "ls");
+        ls.setMode(DigitalChannel.Mode.INPUT);
+
+
+        //extender_L.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //extender_R.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //extender_L.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //extender_R.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         DcMotor currentmotor;
 
@@ -43,8 +49,6 @@ public class auto_red_right extends LinearOpMode {
             currentmotor = extender_L;
             telemetry.addData("Active Motor", "Left Motor");
         }
-        int pos = currentmotor.getCurrentPosition(); // References encoder position from the currently using side
-
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
@@ -53,39 +57,40 @@ public class auto_red_right extends LinearOpMode {
         drive.setPoseEstimate(startPose);
 
         TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
-                //.strafeTo(new Vector2d(4,-27))
+                .addDisplacementMarker(1,() ->{
+                    sv_1.setPosition(0);
+                    sv_2.setPosition(1);
+                    currentmotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    currentmotor.setPower(-1);
+                    if (!ls.getState()){
+                        currentmotor.setPower(0);
+                        currentmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                        currentmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    }
+                })
+
                 .addDisplacementMarker(3, () -> {
                     currentmotor.setTargetPosition(4000);
                     currentmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                     currentmotor.setPower(1);
                 })
 
-                .addSpatialMarker(new Vector2d(4, -27), () -> {
+                .addSpatialMarker(new Vector2d(0, -31), () -> {
                     currentmotor.setTargetPosition(3300);
                     currentmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    currentmotor.setPower(1);
-                    if (currentmotor.getCurrentPosition() < 4000){
-                        sv_2.setPosition(0.4);
-                    }
+                    currentmotor.setPower(-1);
+                    sv_2.setPosition(0.4);
                 })
 
-                .addSpatialMarker(new Vector2d(46, -60), () -> {
-
+                .addSpatialMarker(new Vector2d(46, -45), () -> {
                     sv_2.setPosition(0);
                 })
-
-                //.back(10)
-                //.strafeRight(25)
-                //.splineToLinearHeading(new Pose2d(46, -6,Math.toRadians(180)),Math.toRadians(0))
-                //.strafeLeft(50)
-                //.strafeRight(50)
-                //.back(9)
-                //.strafeLeft(50)
-                //.strafeRight(50)
-                //.back(3)
-                //.strafeLeft(50)
-
                 .strafeTo(new Vector2d(4,-27))
+
+                .addDisplacementMarker(() -> {
+                    sv_2.setPosition(0.4);
+                })
+
                 .back(10)
                 .strafeRight(25)
                 .splineToLinearHeading(new Pose2d(46, -6,Math.toRadians(270)),Math.toRadians(0))
