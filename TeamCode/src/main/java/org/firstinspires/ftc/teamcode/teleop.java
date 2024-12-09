c1aa7cdca9609250a226604ade2a8b23340b1e24
localenstarrie <peal.sima@gmail.com>
2024-12-09 17:44:59 +0700

    sensitivity 0.05 -> 0.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -43,7 +43,7 @@ public class teleop extends OpMode
     boolean invertY = true;
     double deadZone = 0.1;
     double saturation = 1;
-    double sensitivity = 0.05;
+    double sensitivity = 0.1;
     double range = 1;
     public boolean manual = false;
 
