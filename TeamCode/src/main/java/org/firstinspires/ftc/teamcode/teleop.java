36497e6073c5fee44df4d43ba888f6aaac4cd456
piyawat luknatin <125401439+piyawatluk@users.noreply.github.com>
2024-12-10 16:49:07 +0700

    remove unused module,rename variables and รวบ function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -1,39 +1,35 @@
-package org.firstinspires.ftc.teamcode;
+//อิติปิโส ภะคะวา อะระหัง สัมมาสัมพุทโธ, วิชชาจะระณะสัมปันโน, สุขโต โลกะวิทู, อนุตตะโร ปุริสสะทัมมะสาระถิ, สัตถา เทวะมะนุสสานัง, พุทโธ ภะคะวาติ
 
-import static java.lang.Thread.sleep;
+//@piyawat luknatin, worawan simabowonsut LSP robotics 2024-2025
+
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.ftccommon.SoundPlayer;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.cordinate_converter;
 
-
-@TeleOp(name="teleop please run this", group="Iterative OpMode")
+@TeleOp(name="TELEOP!!!", group="Iterative OpMode")
 public class teleop extends OpMode
 {
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor FLM = null;
-    private DcMotor BLM = null;
-    private DcMotor FRM = null;
-    private DcMotor BRM = null;
+    private DcMotor FRONT_LEFT_MOTOR = null;
+    private DcMotor BACK_LEFT_MOTOR = null;
+    private DcMotor FRONT_RIGHT_MOTOR = null;
+    private DcMotor BACK_RIGHT_MOTOR = null;
 
-    private Servo sv_1, sv_3, sv_2;
-    private CRServo sv_4;
+    private Servo LEFT_ARM_SERVO, RIGHT_ARM_SERVO, GRIPPER_SERVO;
+    private CRServo INTAKE_SERVO;
     private DcMotor extender_L = null;
     private DcMotor extender_R = null;
-    private DigitalChannel ls = null;
-    private DcMotor currentmotor;
-    private boolean soundFound;
+    private DigitalChannel LIMIT_SWITCH = null;
+    private DcMotor CURRENT_MOTOR;
 
     //setting arm servo position in degree
-    double pos4 = (double) 170 / 180;
     double pos3 = (double) 100 / 180;
     double pos2 = (double) 15 / 180;
     double pos1 = (double) 10 / 180;
@@ -46,51 +42,31 @@ public class teleop extends OpMode
     double sensitivity = 0.1;
     double range = 1;
     public boolean manual = false;
-
-    public boolean rightmotor = false;
+    public boolean RIGHT_MOTOR = false;
 
     @Override
     public void init() {
 
-
-
-
         extender_L = hardwareMap.get(DcMotor.class, "et_1");
         extender_R = hardwareMap.get(DcMotor.class,  "et_2");
-
-        if(rightmotor){
-            currentmotor = extender_R;
-            telemetry.addData("Active Motor", "Right Motor");
-        }
-        else{
-            currentmotor = extender_L;
-            telemetry.addData("Active Motor", "Left Motor");
-        }
 
         telemetry.addData("Status", "Initialized");
 
         // Initialize motors
-        FLM = hardwareMap.get(DcMotor.class, "FLM");
-        BLM = hardwareMap.get(DcMotor.class, "BLM");
-        FRM = hardwareMap.get(DcMotor.class, "FRM");
-        BRM = hardwareMap.get(DcMotor.class, "BRM");
-
-
+        FRONT_LEFT_MOTOR = hardwareMap.get(DcMotor.class, "FLM");
+        BACK_LEFT_MOTOR = hardwareMap.get(DcMotor.class, "BLM");
+        FRONT_RIGHT_MOTOR = hardwareMap.get(DcMotor.class, "FRM");
+        BACK_RIGHT_MOTOR = hardwareMap.get(DcMotor.class, "BRM");
 
         //arm section
-        sv_1 = hardwareMap.get(Servo.class, "sv_1");
-        sv_2 = hardwareMap.get(Servo.class, "sv_2");
-        sv_3 = hardwareMap.get(Servo.class, "sv_3");
-        sv_4 = hardwareMap.get(CRServo.class, "sv_4");
-
-
-        ls = hardwareMap.get(DigitalChannel.class, "ls");
-
-        //initially stop the intake motor
-        sv_4.setPower(0);
+        LEFT_ARM_SERVO = hardwareMap.get(Servo.class, "sv_1");
+        GRIPPER_SERVO = hardwareMap.get(Servo.class, "sv_2");
+        RIGHT_ARM_SERVO = hardwareMap.get(Servo.class, "sv_3");
+        INTAKE_SERVO = hardwareMap.get(CRServo.class, "sv_4");
+        LIMIT_SWITCH = hardwareMap.get(DigitalChannel.class, "ls");
 
         //hardware behavior declaration for lift system
-        ls.setMode(DigitalChannel.Mode.INPUT);
+        LIMIT_SWITCH.setMode(DigitalChannel.Mode.INPUT);
 
         extender_L.setDirection(DcMotor.Direction.FORWARD);
         extender_R.setDirection(DcMotor.Direction.REVERSE);
@@ -98,34 +74,22 @@ public class teleop extends OpMode
         extender_L.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         extender_R.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-
-
         // Set motor directions
-        FLM.setDirection(DcMotor.Direction.REVERSE);
-        BLM.setDirection(DcMotor.Direction.REVERSE);
-        FRM.setDirection(DcMotor.Direction.FORWARD);
-        BRM.setDirection(DcMotor.Direction.FORWARD);
+        FRONT_LEFT_MOTOR.setDirection(DcMotor.Direction.REVERSE);
+        BACK_LEFT_MOTOR.setDirection(DcMotor.Direction.REVERSE);
+        FRONT_RIGHT_MOTOR.setDirection(DcMotor.Direction.FORWARD);
+        BACK_RIGHT_MOTOR.setDirection(DcMotor.Direction.FORWARD);
 
-        sv_1.setPosition(0);
-        sv_3.setPosition(1);
+        //initially stop the intake motor
+        INTAKE_SERVO.setPower(0);
 
-
+        ACTIVE_MOTOR_CHECKER();
 
     }
 
     @Override
     public void init_loop() {
-        if (!ls.getState()){
-            currentmotor.setPower(0);
-            currentmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        }
-
-        else if (ls.getState()) {
-            currentmotor.setPower(-0.5);
-        }
-
-        telemetry.addData("ls", ls.getState());
-        telemetry.addData("power", currentmotor.getPower());
+        ENCODER_RESET();
     }
 
     @Override
@@ -134,89 +98,94 @@ public class teleop extends OpMode
 
     }
 
-    public int extender_func(int targetPosition,double power) { // Takes targetPosition as input
+    public void ACTIVE_MOTOR_CHECKER(){
+        if(RIGHT_MOTOR){
+            CURRENT_MOTOR = extender_R;
+            telemetry.addData("Active Motor", "Right Motor");
+        } else{
+            CURRENT_MOTOR = extender_L;
+            telemetry.addData("Active Motor", "Left Motor");
+        }
+    }
 
-        int pos = currentmotor.getCurrentPosition(); // References encoder position from the currently using side
+    public void ENCODER_RESET(){
+        LEFT_ARM_SERVO.setPosition(0);
+        RIGHT_ARM_SERVO.setPosition(1);
+        if (!LIMIT_SWITCH.getState()){
+            CURRENT_MOTOR.setPower(0);
+            CURRENT_MOTOR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        } else {
+            CURRENT_MOTOR.setPower(-0.5);
+        }
+    }
+
+    public void EXTENDER_FUNC(int targetPosition,double power) { // Takes targetPosition as input
+
+        int pos = CURRENT_MOTOR.getCurrentPosition(); // References encoder position from the currently using side
 
         // Set target positions for both motors
-        currentmotor.setTargetPosition(targetPosition);
-        currentmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
+        CURRENT_MOTOR.setTargetPosition(targetPosition);
+        CURRENT_MOTOR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Adjust power based on current position relative to the target
         if (pos < targetPosition) {
-            currentmotor.setPower(power);
+            CURRENT_MOTOR.setPower(power);
         } else if (pos > targetPosition) {
-            currentmotor.setPower(-power);
+            CURRENT_MOTOR.setPower(-power);
         } else { // Target reached
-            currentmotor.setPower(0);
+            CURRENT_MOTOR.setPower(0);
         }
 
         if (manual){
-            currentmotor.setPower(gamepad2.left_stick_y);
+            CURRENT_MOTOR.setPower(gamepad2.left_stick_y);
         }
 
         if (!manual){
-            currentmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            CURRENT_MOTOR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
-
-
-        return pos; // Return the current position
     }
 
     private void movement_presets() {
 
         if(gamepad1.y){
-            extender_func(0,1);
-            sv_1.setPosition(pos3);
-            sv_3.setPosition(1.0 - pos3); //sample collect
+            EXTENDER_FUNC(0,1);
+            LEFT_ARM_SERVO.setPosition(pos3);
+            RIGHT_ARM_SERVO.setPosition(1.0 - pos3); //sample collect
         }
         else if(gamepad1.b){
-            extender_func(660,1);
-            sv_1.setPosition(pos1);
-            sv_3.setPosition(1.0 - pos1); //specimen collect
+            EXTENDER_FUNC(660,1);
+            LEFT_ARM_SERVO.setPosition(pos1);
+            RIGHT_ARM_SERVO.setPosition(1.0 - pos1); //specimen collect
         }
         else if(gamepad1.a){
-            extender_func(6200,1);
-            sv_1.setPosition(pos2);
-            sv_3.setPosition(1.0 - pos2); //high basket
+            EXTENDER_FUNC(6200,1);
+            LEFT_ARM_SERVO.setPosition(pos2);
+            RIGHT_ARM_SERVO.setPosition(1.0 - pos2); //high basket
         }
         else if(gamepad1.dpad_down){
-            extender_func(1900,1);
-            sv_1.setPosition(pos2);
-            sv_3.setPosition(1.0 - pos2); //low basket
+            EXTENDER_FUNC(1900,1);
+            LEFT_ARM_SERVO.setPosition(pos2);
+            RIGHT_ARM_SERVO.setPosition(1.0 - pos2); //low basket
         }
 
-
-
-        if (gamepad1.dpad_left){
-            extender_func(4600,1); // high chamber
+        else if (gamepad1.dpad_left){
+            EXTENDER_FUNC(4600,1); // high chamber
         }
 
-        else if(gamepad1.dpad_up){
-            extender_func(3000,1); // ascend
-        }
     }
 
     private void controlCRServo() {
         if (gamepad1.left_bumper) {
-            sv_4.setPower(1);
+            INTAKE_SERVO.setPower(1);
         } else if (gamepad1.left_trigger > 0.1) {
-            sv_4.setPower(-1);
+            INTAKE_SERVO.setPower(-1);
         } else {
-            sv_4.setPower(0); // Stop servo if no input
+            INTAKE_SERVO.setPower(0); // Stop servo if no input
         }
     }
 
-    private void gripper(double override){
-        double triggerValue = gamepad1.right_trigger;
-
-        double gripperpos = Math.min(triggerValue, 0.4);
-        sv_2.setPosition(gripperpos);
-        if (triggerValue == 0 && override > 0){
-            sv_2.setPosition(override);
-        }
+    private void gripper(){
+        GRIPPER_SERVO.setPosition(Math.min(gamepad1.right_trigger, 0.4));
     }
 
     public void move_func(){
@@ -225,49 +194,41 @@ public class teleop extends OpMode
         double computedY_l = cordinate_converter.computeY(gamepad1.left_stick_x, gamepad1.left_stick_y, deadZone, saturation, sensitivity, range, invertX, invertY);
         double computedX_r = cordinate_converter.computeX(gamepad1.right_stick_x, gamepad1.right_stick_y, deadZone, saturation, sensitivity, range, invertX, invertY);
 
-        // Mecanum drive equations to calculate motor powers
+        // apply mecanum drive equations to calculate motor powers
         double frontLeft = computedY_l + computedX_l + computedX_r;
         double frontRight = computedY_l - computedX_l - computedX_r;
         double backLeft = computedY_l - computedX_l + computedX_r;
         double backRight = computedY_l + computedX_l - computedX_r;
 
         // Apply motor power values
-        FLM.setPower(frontLeft);
-        FRM.setPower(frontRight);
-        BLM.setPower(backLeft);
-        BRM.setPower(backRight);
+        FRONT_LEFT_MOTOR.setPower(frontLeft);
+        FRONT_RIGHT_MOTOR.setPower(frontRight);
+        BACK_LEFT_MOTOR.setPower(backLeft);
+        BACK_RIGHT_MOTOR.setPower(backRight);
     }
     public void auto_spec(){
         if (gamepad1.x){
-            DcMotor currentmotor;
-            extender_func(2900,1);
-            if(rightmotor){
-                currentmotor = extender_R;
-                telemetry.addData("Active Motor", "Right Motor");
-            }
-            else{
-                currentmotor = extender_L;
-                telemetry.addData("Active Motor", "Left Motor");
-            }
+            EXTENDER_FUNC(2900,1);
+        }
+    }
 
-
+    public void OVERRIDE_SYSTEM(){
+        if (gamepad2.right_bumper){
+            ENCODER_RESET();
+        } else if (gamepad2.left_trigger >= 0.7 && gamepad2.right_trigger >= 0.7){
+            manual = true;
         }
     }
 
     @Override
     public void loop() {
-        if (!ls.getState()) {
-            currentmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        }
-        if (gamepad2.left_trigger >= 0.7 && gamepad2.right_trigger >= 0.7){
-            manual = true;
-        }
 
         movement_presets();
         controlCRServo();
         move_func();
-        gripper(0);
+        gripper();
         auto_spec();
+        OVERRIDE_SYSTEM();
 
         telemetry.addData("Pos_L", extender_L.getCurrentPosition());
         telemetry.addData("Pos_R", extender_R.getCurrentPosition());
